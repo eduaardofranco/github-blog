3d0d28331cbd38a1204c2b591332d7a0a9651c9e
eduaardofranco <eduaardo.franco@gmail.com>
2024-03-06 19:53:15 +0000

    target blank footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 export function Footer() {
     return(
         <Container>
-            <p>{'{'} developed by <a href="https://github.com/eduaardofranco">@eduaardofranco</a> {'}'}</p>
+            <p>{'{'} developed by <a href="https://github.com/eduaardofranco" target='blank'>@eduaardofranco</a> {'}'}</p>
         </Container>
     )
 }
