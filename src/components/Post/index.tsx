4be182ac85e6f9a4f9f59a4571ac077fd98d0d63
eduaardofranco <eduaardo.franco@gmail.com>
2024-03-11 13:30:58 +0000

    add date fns

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,4 +1,5 @@
 import { Container, Description, Title } from './styles'
+import { formatDistanceToNow } from 'date-fns'
 
 export function Post( { item } ) {
     console.log(item)
@@ -6,7 +7,7 @@ export function Post( { item } ) {
         <Container>
             <Title>
                 <h3>{item.title}</h3>
-                <span>{item.updated_at}</span>
+                <span>{formatDistanceToNow(item.updated_at)}</span>
             </Title>
             <Description>
                 {item.body}
